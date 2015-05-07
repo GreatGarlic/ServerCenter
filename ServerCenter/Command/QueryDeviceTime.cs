bbb7562482472ce493cd85e9d6fe8d016d09173f
刘源 <405653510@qq.com>
2015-05-07 14:53:40 +0800

    设备时间响应处理

--- ServerCenter/Command/QueryDeviceTime.cs
+++ ServerCenter/Command/QueryDeviceTime.cs
@@ -1,7 +1,10 @@
-﻿using SuperSocket.SocketBase.Command;
+﻿using log4net;
+using ServerCenter.Convert;
+using SuperSocket.SocketBase.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ServerCenter.Command
@@ -11,12 +14,22 @@ namespace ServerCenter.Command
      */
     public class QueryDeviceTime : CommandBase<RainfallSession, RainfallRequestInfo>
     {
+        private static ILog LOGGER = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public override string Name { get { return "FE"; } }
         public override void ExecuteCommand(RainfallSession session, RainfallRequestInfo requestInfo)
         {
-           
-         
+            LOGGER.Debug("===================读取设备时间响应处理====================");
+            int offset = 0;
+            byte[] body = requestInfo.Body;
+            int year = ConvertUtil.byteToBCD(body[offset++])+2000;
+            int month = ConvertUtil.byteToBCD(body[offset++]);
+            int day = ConvertUtil.byteToBCD(body[offset++]);
+            int hour = ConvertUtil.byteToBCD(body[offset++]);
+            int minute = ConvertUtil.byteToBCD(body[offset++]);
+            int second = ConvertUtil.byteToBCD(body[offset++]);
+            DateTime dt = new DateTime(year, month, day, hour, minute, second);
+            LOGGER.Debug("设备时间:"+dt.ToString("yyyy年MM月dd日hh:mm:ss"));
         }
     }
 }
---
+++ ServerCenter/Convert/ConvertUtil.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerCenter.Convert
+{
+    class ConvertUtil
+    {
+        /**
+	     * 
+	     * BCD码转换int型
+	     * 
+	     * 方法添加日期: 2013-7-29 创建者:刘源
+	     */
+        public static int byteToBCD(byte val)
+        {
+            return ((val >> 4) * 10 + (val & 0x0f));
+        }
+        /**
+	     * 
+	     * Int转换BCD码(byte数组)
+	     * 
+	     * 方法添加日期: 2013-9-4 创建者:刘源
+	     */
+        public static byte[] Int2Bcd(int n, int dec)
+        {
+            byte high, low;
+            byte[] buf = new byte[n];
+            for (int i = 0; i < n; i++)
+            {
+                low = (byte)(dec % 10);
+                dec /= 10;
+                high = (byte)(dec % 10);
+                dec /= 10;
+                buf[i] = (byte)((high << 4) + (low & 0x0f));
+            }
+            return buf;
+        }
+        /**
+         * 
+         * Int转换BCD码(byte)
+         * 
+         * 方法添加日期: 2013-9-4 创建者:刘源
+         */
+        public static byte Int2Bcd(int value)
+        {
+            return Int2Bcd(1, value)[0];
+        }
+    }
+}
