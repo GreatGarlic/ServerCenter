8c4aeba973e60f96e371d0249ddae2318f34093b
刘源 <405653510@qq.com>
2015-05-08 17:22:51 +0800

    历史雨量数据业务处理完成

--- ServerCenter/Command/QueryHistoricalRainfall.cs
+++ ServerCenter/Command/QueryHistoricalRainfall.cs
@@ -1,8 +1,14 @@
-﻿using SuperSocket.SocketBase.Command;
+﻿using log4net;
+using SuperSocket.SocketBase.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using SuperSocket.Common;
+using ServerCenter.Entity;
+using ServerCenter.Convert;
+
 
 namespace ServerCenter.Command
 {
@@ -11,10 +17,28 @@ namespace ServerCenter.Command
     */
     public class QueryHistoricalRainfall : CommandBase<RainfallSession, RainfallRequestInfo>
     {
+        private static ILog LOGGER = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         public override string Name { get { return "FA"; } }
         public override void ExecuteCommand(RainfallSession session, RainfallRequestInfo requestInfo)
         {
-            throw new NotImplementedException();
+            //数据包数量
+            uint countPackage = (uint)(requestInfo.Body[0] & 0xFF);
+            byte[] contextBytes = requestInfo.Body.CloneRange(1, requestInfo.Body.Length - 1);
+            if (contextBytes.Length == countPackage * 7)
+            {
+                List<HistoricalRainfall> dataList = new List<HistoricalRainfall>();
+                for (int i = 0; i < countPackage; i++)
+                {
+                    byte[] dataByte = contextBytes.CloneRange(i * 7, 7);
+                    HistoricalRainfall data = new HistoricalRainfall();
+                    data.historicalTime = ConvertUtil.Bcd2Date(dataByte.CloneRange(0, 6));
+                    data.TipperCount = (uint)(dataByte[6] & 0xFF);
+                    dataList.Add(data);
+                }
+                //正常收到数据包的回复
+                byte[] responseData = { 0x38 };
+                session.Send(responseData, 0, 1);
+            }
         }
     }
 }
---
+++ ServerCenter/Entity/HistoricalRainfall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerCenter.Entity
+{
+    /*
+     * 雨量历史数据实体对象.
+     */
+    class HistoricalRainfall
+    {
+        /*
+         * 历史时间.
+         */ 
+        public DateTime historicalTime { get; set; }
+        /*
+         * 翻斗数.
+         */
+        public uint TipperCount { get; set; }
+
+    }
+}
