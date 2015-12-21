9eccc233388b6b846466c00c04a7dd650d4d398a
刘源 <405653510@qq.com>
2015-12-21 09:37:49 +0800

    提交

--- Protocol/RainfallServer.cs
+++ Protocol/RainfallServer.cs
@@ -1,4 +1,5 @@
 ﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,30 @@ namespace Protocol
             : base(new RainfallReceiveFilterFactory<RainfallRequestInfo>())
         {
 
+        }
+        /// <summary>
+        /// 读取你的自定义配置信息.
+        /// </summary>
+        /// <param name="rootConfig"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        protected override bool Setup(IRootConfig rootConfig, IServerConfig config)
+        {
+            return base.Setup(rootConfig, config);
+        }
+        /// <summary>
+        /// 服务启动时.
+        /// </summary>
+        protected override void OnStarted()
+        {
+            base.OnStarted();
+        }
+        /// <summary>
+        /// 服务关闭时.
+        /// </summary>
+        protected override void OnStopped()
+        {
+            base.OnStopped();
         }
     }
 }
